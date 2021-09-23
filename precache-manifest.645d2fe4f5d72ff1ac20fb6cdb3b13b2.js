e37a5e07efb22eb476bda1f57fe6b80239da06a0
dyCodes <yusufdaudu51@gmail.com>
2021-09-23 05:14:46 +0100

    Updates

---
+++ precache-manifest.645d2fe4f5d72ff1ac20fb6cdb3b13b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004cfd5cae315324de2c29984fea5fe0",
+    "url": "/todolist-reactjs/index.html"
+  },
+  {
+    "revision": "86aa1afc359ce54f3c88",
+    "url": "/todolist-reactjs/static/css/main.0630cf0f.chunk.css"
+  },
+  {
+    "revision": "45b1192cb663df06339a",
+    "url": "/todolist-reactjs/static/js/2.694cf2b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-reactjs/static/js/2.694cf2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86aa1afc359ce54f3c88",
+    "url": "/todolist-reactjs/static/js/main.0db161c4.chunk.js"
+  },
+  {
+    "revision": "b4c6b84fc9ea7fdebe5b",
+    "url": "/todolist-reactjs/static/js/runtime-main.2384774a.js"
+  }
+]);
